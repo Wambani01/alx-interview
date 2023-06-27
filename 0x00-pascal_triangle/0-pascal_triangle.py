907401152a2c80f1bf4c310743a89773f18a5fc9
Wambani01 <georgewmbn@gmail.com>
2023-06-27 12:09:26 -0700

    pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    if n == 1:
+        return [[1]]
+    if n == 2:
+        return [[1], [1, 1]]
+    if n > 2:
+        base = [[1], [1, 1]]
+        for i in range(n - 2):
+            last = base[-1]
+            next = [1]
+            for i in range(len(last) - 1):
+                next.append(last[i] + last[i + 1])
+            next.append(1)
+            base.append(next)
+    return base
